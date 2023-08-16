46926819cc78b83454ef02fe3f02c2a0abf623d1
brina <brina@sc-company.co.kr>
2023-08-16 17:06:58 +0900

    Feat: main UI Upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,6 @@
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 };
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
