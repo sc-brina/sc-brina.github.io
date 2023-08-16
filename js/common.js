7ae204c4d11ed4391009ca1976b7fd9b53e55b90
brina <brina@sc-company.co.kr>
2023-08-16 19:20:53 +0900

    Feat: main UI Upload

--- js/common.js
+++ js/common.js
@@ -2,5 +2,31 @@ if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 };
 
+AOS.init();
+// aos
+
+const toTopEl = document.querySelector('#to-top');
+window.addEventListener('scroll', _.throttle(function () {
+  //_.throttle(함수, 시간)
+  //gsap.to(요소, 지속시간, 옵션)
+  console.log(window.scrollY);
+  if (window.scrollY > 400) {
+    gsap.to('#to-top', 0.2, {
+      x: 0
+    });
+  } else {
+    gsap.to('#to-top', 0.2, {
+      x: 100
+    });
+  }
+}, 300));
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, 0.5, {
+    scrollTo: 0
+  });
+});
+// to-top
+
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
+// footer
