8280b03f542aabb41cc88f94555c98d94ecf93bb
Brina <brina@sc-company.co.kr>
2023-08-17 11:42:48 +0900

    UI: Upload modifications

--- js/membership.js
+++ js/membership.js
@@ -4,10 +4,10 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 
 $('.chart').easyPieChart({
   barColor: '#000',  //차트가 그려질 색
-  trackColor: '#f4f4f4',  // 차트가 그려지는 트랙의 기본 배경색(chart1 의 회색부분)
+  trackColor: '#ccc',  // 차트가 그려지는 트랙의 기본 배경색(chart1 의 회색부분)
   lineCap: 'butt', // 차트 선의 모양 chart1 butt / chart2 round / chart3 square
-  lineWidth: 10, // 차트 선의 두께
-  size: 200, // 차트크기
+  lineWidth: 15, // 차트 선의 두께
+  size: 300, // 차트크기
   animate: 1000, // 그려지는 시간 
   onStart: $.noop,
   onStop: $.noop
