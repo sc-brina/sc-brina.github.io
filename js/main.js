0b6c9010b8b6cfaf2346a1cac01c9c3c51824a8a
Brina <brina@sc-company.co.kr>
2023-09-07 08:32:21 +0900

    UI: Modify Image / Main Page

--- js/main.js
+++ js/main.js
@@ -29,4 +29,29 @@ var swiper = new Swiper(".mainSwiper", {
     el: ".swiper-pagination",
   },
 });
-// 
+// main
+
+var swiper = new Swiper(".instagramSwiper", {
+  slidesPerView: 4.5,
+  spaceBetween: 10,
+  loop: true,
+  loopAdditionalSlides: 1,
+  threshold: 100,
+  speed: 300,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1921: {
+      slidesPerView: 5.5,
+    },
+    641: {
+      slidesPerView: 4.5,
+    },
+    0: {
+      slidesPerView: 2.5,
+    },
+  },
+});
+// instagram
